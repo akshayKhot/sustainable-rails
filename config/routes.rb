c70667bc34a7b0411c6e5df20e83ee634e01c417
Akshay Khot <akshay.khot@hey.com>
2021-09-11 14:12:54 +0530

    rating

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  resources :widgets, only: %i{ show index }
+  resources :widgets, only: %i[ show index ]
+  resources :widget_ratings, only: %i[ create ]
   
   # Custom Routes
   get "/amazing", to: "widgets#index"
